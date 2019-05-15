53b260cae576431df35de5e5e54e77855fc64e69
Your Name <rodrigobsi@usp.br>
2019-05-15 12:50:46 -0300

    Testando o branch

--- aplicacao/sustainable/views.py
+++ aplicacao/sustainable/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-
 # Create your views here.
 def index(request):
     exemplo_de_variavel_no_template = "Essa String do botão veio da função 'index' sustainable/views.py"
@@ -8,7 +7,6 @@ def index(request):
     context = {
         "texto_qualquer": exemplo_de_variavel_no_template
     }
-
     return render(request, 'index.html', context)
 
 
