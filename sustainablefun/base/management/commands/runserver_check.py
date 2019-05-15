748f32c6515c59e1eab7690f2145edf30a33182b
felipearmat <felipearmat@gmail.com>
2019-05-14 23:24:05 -0300

    Linting do código.

--- sustainablefun/base/management/commands/runserver_check.py
+++ sustainablefun/base/management/commands/runserver_check.py
@@ -11,27 +11,27 @@ from time import sleep
 from django.contrib.staticfiles.management.commands.runserver import Command as StaticfilesRunserverCommand
 
 def printcounter():
-  print("\n****************************************")
-  for i in range(45, -1, -1):
-    print(f"Prospector will be ready in (about) {i} seconds!", end = '\r')
-    sleep(1)
+    print("\n****************************************")
+    for i in range(3, -1, -1):
+        print(f"Prospector will be ready in (about) {i} seconds!", end = '\r')
+        sleep(1)
 
 class Command(StaticfilesRunserverCommand):
-  help = "Starts a lightweight Web server for development, serves static files and does some custom fancy stuff."
+    help = "Starts a lightweight Web server for development, serves static files and does some custom fancy stuff."
 
-  def run_prospector(self):
-    """Runs prospector on codebase with a subprocess, while start a counter
-    on another so server can get running while prospector check .py files"""
-    subprocess.Popen(['prospector'])
-    p = Process(target=printcounter)
-    p.start()
+    def run_prospector(self):
+        """Runs prospector on codebase with a subprocess, while start a counter
+        on another so server can get running while prospector check .py files"""
+        subprocess.Popen(['prospector'])
+        p = Process(target=printcounter)
+        p.start()
 
-  def add_arguments(self, parser):
-    super(Command, self).add_arguments(parser)
+    def add_arguments(self, parser):
+        super(Command, self).add_arguments(parser)
 
-  def get_handler(self, *args, **options):
-    """Overriden get handler method, call run_prospector for static analysis of code
-    """
-    handler = super(Command, self).get_handler(*args, **options)
-    self.run_prospector()
-    return handler
+    def get_handler(self, *args, **options):
+        """Overriden get handler method, call run_prospector for static analysis of code
+        """
+        handler = super(Command, self).get_handler(*args, **options)
+        self.run_prospector()
+        return handler
