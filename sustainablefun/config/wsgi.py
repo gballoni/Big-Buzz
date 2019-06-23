051060cfe7085d68163d6fd9959e994e00d0d5e7
felipearmat <felipearmat@gmail.com>
2019-06-23 18:11:26 -0300

    Ajustes para colocar projeto em produção

--- sustainablefun/config/wsgi.py
+++ sustainablefun/config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for aplicacao project.
+WSGI config for sustainablefun project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aplicacao.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sustainablefun.config.settings')
 
 application = get_wsgi_application()
